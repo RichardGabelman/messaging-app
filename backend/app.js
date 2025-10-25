fb1b80884959c50ce52c6fe3c3c85c2ec898b4f6
Richard Gabelman <78068360+RichardGabelman@users.noreply.github.com>
2025-10-24 18:27:07 -0700

    feat: add basic express app.js file

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,28 @@
+const express = require("express");
+const apiRouter = require("./src/routes/api.js");
+const cors = require("cors");
+
 require("dotenv").config();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors({ origin: "http://localhost:5173" }));
+
+app.use("/api", apiRouter);
+
+app.use((req, res) => {
+  res.status(404).json({ error: "Not found" });
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.statusCode || 500).json({ error: err.message });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Messaging-App API - listening on port ${PORT}!`);
+});

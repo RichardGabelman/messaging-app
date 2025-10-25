4f1439e141fa3279055eacff192da5b8cc70afa4
Richard Gabelman <78068360+RichardGabelman@users.noreply.github.com>
2025-10-24 18:43:35 -0700

    feat: implement basic backend route structure

--- backend/src/routes/api.js
+++ backend/src/routes/api.js
@@ -1,5 +1,14 @@
 const express = require("express");
+const authRouter = require("./auth.js");
+const userRouter = require("./user.js");
+const messageRouter = require("./message.js");
 
 const router = express.Router();
+
+router.use("/auth", authRouter);
+
+router.use("/users", userRouter);
+
+router.use("/messages", messageRouter);
 
 module.exports = router;

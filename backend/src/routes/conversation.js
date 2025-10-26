224e99d2f2b3f6b862685340f5f215a84108e840
Richard Gabelman <78068360+RichardGabelman@users.noreply.github.com>
2025-10-25 19:38:12 -0700

    feat: implement get conversation with otherUserId endpoint

--- backend/src/routes/conversation.js
+++ backend/src/routes/conversation.js
@@ -1,21 +1,58 @@
 const express = require("express");
 const { isLoggedIn, isParticipant } = require("../middlewares/authMiddleware");
+const { user } = require("../db/prisma");
+const prisma = require("../db/prisma");
 
 const router = express.Router();
 
 router.use(isLoggedIn);
 
-// returns (and creates if not already created) the conversation 
+// returns (and creates if not already created) the conversation
 // (perhaps just conversation id) of the conversation between the user and the otherUser
 router.get("/with/:otherUserId", async (req, res, next) => {
-  res.status(501);
+  const otherUserId = parseInt(req.params.otherUserId);
+
+  if (req.user.id === otherUserId) {
+    return res
+      .status(400)
+      .json({ error: "Cannot create conversations with yourself" });
+  }
+  // want the smallest id to be AId
+  const [userAId, userBId] = [req.user.id, otherUserId].sort((a, b) => a - b);
+
+  try {
+    const otherUser = await prisma.user.findUnique({
+      where: { id: otherUserId },
+      select: { id: true, username: true },
+    });
+
+    if (!otherUser) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    const conversation = await prisma.conversation.upsert({
+      where: { userAId_userBId: { userAId, userBId } },
+      create: { userAId, userBId },
+      update: {},
+      include: {
+        messages: {
+          include: { author: { select: { id: true, username: true } } },
+          orderBy: { timestamp: "asc" },
+        },
+        userA: { select: { id: true, username: true } },
+        userB: { select: { id: true, username: true } },
+      },
+    });
+
+    res.status(200).json(conversation);
+  } catch (err) {
+    next(err);
+  }
 });
 
-// posts a new message in the conversation with CONVERSATION ID == :id
-router.post("/:id/messages", isParticipant, async (req, res, next) => {
-  res.status(501);
-});
-
-
+// // posts a new message in the conversation with CONVERSATION ID == :id
+// router.post("/:id/messages", isParticipant, async (req, res, next) => {
+//   res.status(501);
+// });
 
 module.exports = router;
